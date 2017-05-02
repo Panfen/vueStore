1d6154efca07a18259fde275218c482434f92d92
panfen <2809956575@qq.com>
2017-05-02 13:52:46 +0800

    'update'

--- build/dev-server.js
+++ build/dev-server.js
@@ -21,6 +21,35 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+var appData = require('../data.json')
+var seller = appData.seller
+var goods = appData.goods
+var ratings = appData.ratings
+
+var apiRoutes = express.Router()
+
+apiRoutes.get('/seller', function (req, res) {
+  res.json({
+    errno: 0,
+    seller: seller
+  })
+})
+
+apiRoutes.get('/seller', function (req, res) {
+  res.json({
+    errno: 0,
+    seller: seller
+  })
+})
+
+apiRoutes.get('/seller', function (req, res) {
+  res.json({
+    errno: 0,
+    seller: seller
+  })
+})
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
